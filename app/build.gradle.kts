eb6eaf91e521610ad1790b9db313a0f2347f6603
Mert Unluer <mertnler113@gmail.com>
2024-05-09 10:07:46 +0300

    implement library into main app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
         }
     }
@@ -50,6 +49,9 @@ android {
 }
 
 dependencies {
+    implementation("com.github.mrtunluer:CountryCodePicker:1.0.0")
+    //implementation(project(":countrycodepicker"))
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
@@ -58,7 +60,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation(project(":countrycodepicker"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,10 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "CountryCodePıcker"
 include(":app")
-include(":countrycodepicker")
+//include(":countrycodepicker")
